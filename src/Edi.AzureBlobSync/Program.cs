a21fe5094722ee8375464480feb8dd8a482863f3
Edi Wang <Edi.Wang@outlook.com>
2017-12-03 21:30:33 +0800

    First working version...

--- src/Edi.AzureBlobSync/Program.cs
+++ src/Edi.AzureBlobSync/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CommandLine;
@@ -15,7 +17,7 @@ namespace Edi.AzureBlobSync
 
         [Option('k', Required = true, HelpText = "Storage Account Key")]
         public string AccountKey { get; set; }
-        
+
         [Option('c', Required = true, HelpText = "Blob Container Name")]
         public string ContainerName { get; set; }
 
@@ -23,11 +25,25 @@ namespace Edi.AzureBlobSync
         public string LocalFolderPath { get; set; }
     }
 
-    class BlobFile
+    class FileSyncInfo
     {
         public string FileName { get; set; }
 
-        public DateTimeOffset? LastModified { get; set; }
+        public long Length { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is FileSyncInfo si)
+            {
+                return si.FileName == FileName && si.Length == Length;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return $"{FileName}{Length}".Length;
+        }
     }
 
     class Program
@@ -43,6 +59,7 @@ namespace Edi.AzureBlobSync
             {
                 Options = ((Parsed<Options>)parserResult).Value;
 
+                // 1. Get Azure Blob Files
                 Console.WriteLine("Finding Files on Azure Blob Storage...");
                 BlobContainer = GetBlobContainer();
                 var blobs = await BlobContainer.ListBlobsSegmentedAsync(null);
@@ -50,16 +67,95 @@ namespace Edi.AzureBlobSync
                                   where item.GetType() == typeof(CloudBlockBlob)
                                   select (CloudBlockBlob)item
                                   into blob
-                                  select new BlobFile()
+                                  select new FileSyncInfo()
                                   {
-                                      LastModified = blob.Properties.LastModified,
-                                      FileName = blob.Name
+                                      FileName = blob.Name,
+                                      Length = blob.Properties.Length
                                   }).ToList();
 
-                Console.WriteLine($"{cloudFiles.Count} file(s) found.");
+                Console.WriteLine($"{cloudFiles.Count} cloud file(s) found.");
 
+                // 2. Get Local Files
+                var localFilePaths = Directory.GetFiles(Options.LocalFolderPath);
+                var localFiles = localFilePaths.Select(filePath => new FileInfo(filePath))
+                                               .Select(fi => new FileSyncInfo
+                                               {
+                                                   FileName = fi.Name,
+                                                   Length = fi.Length,
+                                               })
+                                               .ToList();
+
+                Console.WriteLine($"{localFiles.Count} local file(s) found.");
+
+                // 3. Compare Files
+                Console.WriteLine($"Comparing file meta data...");
+                Console.WriteLine("----------------------------------------------------");
+
+                // Files in cloud but not in local
+                var excepts = cloudFiles.Except(localFiles).ToList();
+                if (excepts.Any())
+                {
+                    Console.WriteLine($"{excepts.Count} new file(s) to download. [ENTER] to continue, other key to cancel.");
+                    var k = Console.ReadKey();
+                    if (k.Key == ConsoleKey.Enter)
+                    {
+                        // Download New Files
+                        var downloadTask = new List<Task>();
+                        foreach (var fileSyncInfo in excepts)
+                        {
+                            Console.WriteLine($"Added {fileSyncInfo.FileName} ({fileSyncInfo.Length} bytes) to download.");
+                            downloadTask.Add(DownloadAsync(fileSyncInfo.FileName));
+                        }
+                        await Task.WhenAll(downloadTask);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"No new files need to be downloaded.");
+                }
+
+                // 5. Ask Delete Old Files
+                var localExcepts = localFiles.Except(cloudFiles).ToList();
+                var deleteCount = 0;
+                if (localExcepts.Any())
+                {
+                    Console.WriteLine($"{localExcepts.Count} redundancy file(s) exists in local but not on cloud, [V] to view file list, [ENTER] to continue.");
+                    var k = Console.ReadKey();
+                    if (k.Key == ConsoleKey.V)
+                    {
+                        foreach (var f in localExcepts)
+                        {
+                            Console.WriteLine($"{f.FileName}\t{f.Length} bytes");
+                        }
+                    }
+                    if (k.Key == ConsoleKey.Enter)
+                    {
+                        Console.WriteLine($"Do you want to delete these files? [Y/N]");
+                        var k1 = Console.ReadKey();
+                        if (k1.Key == ConsoleKey.Y)
+                        {
+                            Console.WriteLine("Deleting local redundancy files...");
+                            foreach (var fi in localExcepts)
+                            {
+                                File.Delete(Path.Combine(Options.LocalFolderPath, fi.FileName));
+                                deleteCount++;
+                            }
+                        }
+                    }
+                }
+
+                Console.WriteLine("----------------------------------------------------");
+                Console.WriteLine($"Local Files Up to Date. {excepts.Count} new file(s) downloaded, {deleteCount} file(s) deleted.");
                 Console.ReadLine();
             }
+        }
+
+        private static async Task DownloadAsync(string remoteFileName)
+        {
+            CloudBlockBlob blockBlob = BlobContainer.GetBlockBlobReference(remoteFileName);
+            var newFilePath = Path.Combine(Options.LocalFolderPath, remoteFileName);
+            await blockBlob.DownloadToFileAsync(newFilePath, FileMode.Create);
+            Console.WriteLine($"[{DateTime.Now}] {remoteFileName} downloaded.");
         }
 
         private static CloudBlobContainer GetBlobContainer()
