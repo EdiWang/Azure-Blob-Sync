f7ffe4aa360e392e2ad33373aedccf05e3d52f4d
Edi Wang <Edi.Wang@outlook.com>
2017-12-03 16:03:58 +0800

    New console project

---
+++ src/Edi.AzureBlobSync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Edi.AzureBlobSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
