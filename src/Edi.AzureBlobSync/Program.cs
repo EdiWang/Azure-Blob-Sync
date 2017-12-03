f8f5629abca5c991cca24a2084413851c4437db0
Edi Wang <Edi.Wang@outlook.com>
2017-12-03 16:55:07 +0800

    add arguments support

--- src/Edi.AzureBlobSync/Program.cs
+++ src/Edi.AzureBlobSync/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using CommandLine;
 
 namespace Edi.AzureBlobSync
 {
+    class Options
+    {
+        [Option('n', Required = true, HelpText = "Storage Account Name")]
+        public string AccountName { get; set; }
+
+        [Option('k', Required = true, HelpText = "Storage Account Key")]
+        public string AccountKey { get; set; }
+
+        [Option('p', Default = "C:\\AzureBlobSyncTemp", Required = true, HelpText = "Local Folder Path")]
+        public string LocalFolderPath { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var parserResult = Parser.Default.ParseArguments<Options>(args);
+            if (parserResult.Tag == ParserResultType.Parsed)
+            {
+                var opts = ((Parsed<Options>)parserResult).Value;
+                
+                Console.WriteLine($"{opts.AccountKey} | {opts.AccountName} | {opts.LocalFolderPath}");
+                Console.ReadLine();
+            }
         }
     }
 }
